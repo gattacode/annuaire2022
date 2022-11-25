5cd4a9ad23b6a3b441c87fcb3f3fa3f69c1ecec3
BA GATTA p2201085 <gatta.ba@etu.univ-lyon1.fr>
2022-11-25 16:04:39 +0000

    Correction des messages affichés

--- sae/main.c
+++ sae/main.c
@@ -13,22 +13,22 @@ int main()
     printf("\t 3.Suppression client\n");
     printf("\t 4.Afficher la liste des clients\n");
     printf("\t 5.Filtrer les clients\n");
-    printf("\t 6.Liste des clients avec donnees manquantes\n");
+    printf("\t 6.Liste des clients avec des donnees manquantes\n");
     printf("\t 7.sauvegarde des donnees\n");
 
-    printf("saisissez une valeur : ");
+    printf("Saisissez une valeur : ");
     scanf("%d", &entrer);
 
     switch (entrer)
     {
-        case 1: printf("vous avez choisi : Ajouter un nouveau client\n"); ajout(); break;
-        case 2: printf("vous avez choisi : Modification client\n"); modif(); break;
-        case 3: printf("vous avez choisi : Suppression client\n"); supp(); break;
-        case 4: printf("vous avez choisi : Afficher la liste des clients\n"); affiche(); break;
-        case 5: printf("vous avez choisi : Filtrer les clients\n"); filtre(); break;
-        case 6: printf("vous avez choisi : Liste des clients avec donnees manquantes\n"); manquant(); break;
-        case 7: printf("vous avez choisi : sauvegarde des donnees\n"); save(); break;
-        default : printf("saisie invalide\n"); break;
+        case 1: printf("Vous avez choisi : Ajouter un nouveau client\n"); ajout(); break;
+        case 2: printf("Vous avez choisi : Modification client\n"); modif(); break;
+        case 3: printf("Vous avez choisi : Suppression client\n"); supp(); break;
+        case 4: printf("Vous avez choisi : Afficher la liste des clients\n"); affiche(); break;
+        case 5: printf("Vous avez choisi : Filtrer les clients\n"); filtre(); break;
+        case 6: printf("Vous avez choisi : Liste des clients avec des donnees manquantes\n"); manquant(); break;
+        case 7: printf("Vous avez choisi : sauvegarde des donnees\n"); save(); break;
+        default : printf("Saisie invalide\n"); break;
     }
 
     return 0;
